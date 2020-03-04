b36d355a5a56d01c834e50f4889107e6ecd4e0fa
haqiuuu <1372789896@qq.com>
2020-03-04 21:36:17 +0800

    首页 banner

---
+++ src/components/home/HomeBanner.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="home-banner" @click="onClick" v-if="img">
+    <div class="bg-img" :style="{backgroundImage:'url('+img+')'}">
+      <div class="title">{{title}}</div>
+      <div class="subtitle-wrapper">
+        <div class="sub-Title">{{subtitle}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    subtitle: {
+      type: String,
+      default: ""
+    },
+    img: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit("onClick");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.home-banner {
+  margin-top: 20px;
+  padding-right: 20px;
+  padding-left: 20px;
+  .bg-img {
+    height: 125px;
+    border-radius: 15px;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    box-sizing: border-box;
+    .title {
+      transform: translateY(30px);
+      text-align: center;
+      color: #fff;
+      font-size: 18px;
+    }
+    .subtitle-wrapper {
+        padding: 0 34%;
+      .sub-Title {
+        transform: translateY(45px);
+        text-align: center;
+        border-radius: 50px;
+        border: 1px solid #ccc;
+        padding: 8px 15px;
+        background-color: #4585ff;
+        color: #fff;
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,20 +2,31 @@
   <div>
     <SearchBar disabled="true" @onClick="onSearchBarkClick" />
     <HomeCard />
+    <HomeBar
+      title="百本热门书籍免费开放"
+      subtitle="立即体验"
+      img="http://www.youbaobao.xyz/book/res/bg.jpg"
+      @onClick="onHomeBarClick"
+    />
   </div>
 </template>
 
 <script>
 import SearchBar from "../../components/home/SearchBar";
 import HomeCard from "../../components/home/homeCard";
+import HomeBar from "../../components/home/HomeBanner";
 export default {
   components: {
     SearchBar,
-    HomeCard
+    HomeCard,
+    HomeBar
   },
   methods: {
     onSearchBarkClick() {
       // 跳转到搜索页面
+    },
+    onHomeBarClick() {
+      console.log("onHomeBarClick");
     }
   }
 };
